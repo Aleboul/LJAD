336f6527d3017576669da2b02a7112db0065f6dd
Aleboul <82594658+Aleboul@users.noreply.github.com>
2021-05-25 10:11:53 +0200

    Add files via upload

--- FMadogram/code/FMadogram.py
+++ FMadogram/code/FMadogram.py
@@ -20,13 +20,17 @@ def ecdf(data, missing):
 	
 def dist(X, lmbd, missing):
 	"""
-		Compute matrix distance
+		Compute matrix of F-Madogram (a une constante pres) using the empirical cumulative distribution function
 		
 		Inputs
 		------
 		X : a matrix composed of ecdf
 		lmbd : a parameter between 0 and 1
 		missing : vector indicate if we have the complete value
+
+		Outputs
+		-------
+		A matrix with quantity equals to 0 if i=j (diagonal) and equals to sum_t=1^T |F(X_t)^{\lambda} - G(Y_t)^{1-\lambda}| if i \neq j
 	"""
 	ncols = X.shape[1]
 	nrows = X.shape[0]
@@ -47,11 +51,16 @@ def dist(X, lmbd, missing):
 	
 def fmado(x, lmbd=0.5):
 	"""
-		This function performs the PAM algorithm based on the F-madogram
+		This function
 		
 		Inputs
 		------
 		x : a matrix
+		lmbd : constant between 0 and 1 use for the lambda F-madogram
+
+		Outputs
+		-------
+		A matrix equals to 0 if i = j and equalts to |F(X)^{\lambda} - G(Y)^{1-\lambda}|
 	"""
 	
 	Nnb = x.shape[1] // 2
@@ -72,6 +81,23 @@ def fmado(x, lmbd=0.5):
 	return Fmado
 
 def simu(target):
+	"""
+		Perform multiple simulation of the estimation of FMadogram with an increasing length of sample
+
+		Inputs
+		------
+
+		target : a list which contain the following parameters
+				- niter = number of replication
+				- simulation = law that generate the data
+				- probs_missing = array that indicate the probabilities of missing
+				- n_sample = array of multiple lengths of sample
+
+		Outputs
+		-------
+		Array containing niter * length(n_sample) estimators of the FMadogram
+
+	"""
 	output = []
 
 	for k in tqdm(range(target['niter'])):
@@ -92,6 +118,23 @@ def simu(target):
 	return output
 
 def simu_proba(target):
+	"""
+		Perform multiple simulation of the FMadogram estimator with an increase of the probability of missing
+
+		Inputs
+		------
+
+		target : a list which contain the following parameters
+				- niter = number of replication
+				- simulation = law that generate the data
+				- probs_missing = array that indicate the multiple probabilities of missing
+				- n_sample = length of sample
+
+		Outputs
+		-------
+		Array containing niter * length(probs_missing) estimators of the FMadogram
+	"""
+
 	output = []
 	for k in tqdm(range(target['niter'])):
 		probs = []
